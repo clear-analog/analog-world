0efcc12c50f6941336373bd05e6840438882460c
analog-clear <analogclarityworld@gmail.com>
2024-12-12 16:05:19 -0500

    made boilerplate project

---
+++ codebase/codebase/main.c
@@ -0,0 +1,18 @@
+/*
+ * codebase.c
+ *
+ * Created: 12/12/2024 4:04:31 PM
+ * Author : suraj
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
